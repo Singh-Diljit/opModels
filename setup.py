dc2f13dd581133156a15d7d7c4c1babedbc49d74
Singh-Diljit <90169508+Singh-Diljit@users.noreply.github.com>
2024-11-14 13:01:56 -0500

    updated requirements and setup

--- setup.py
+++ setup.py
@@ -5,20 +5,22 @@ setup(
     version='0.1.0',                        
     author='Diljit Singh',                  
     author_email='diljitsingh22 @Googles email service',
-    description='A quantitative finance library for option pricing and analysis',
+    description='Quantitative finance library for option pricing and analysis.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/Singh-Diljit/opModels',
     packages=find_packages(),                
     classifiers=[                           
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.12.3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',                
     install_requires=[                      
-        'numpy>=1.26.0',
-        'scipy>=1.11.3',
-        'pickle5>=1.0.2',
+        'numpy>=1.26.4',
+        'scipy>=1.13.1',
+        'matplotlib>=3.9.0'
+        'pandas>=2.2.2'
+        'yfinance>=0.2.49'
     ],
 )
