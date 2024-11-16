86464a7e1185396f310dad46c9f6e9d792df929a
Singh-Diljit <90169508+Singh-Diljit@users.noreply.github.com>
2024-11-16 06:11:33 -0500

    Add files via upload

--- qf/helperFuncs/formatting.py
+++ qf/helperFuncs/formatting.py
@@ -84,3 +84,61 @@ def callMulti(f):
     else:
         res = lambda X: np.array([[ff(X) for ff in f_i] for f_i in f])
     return res
+
+def paddedVec(X, desiredLen):
+    X = makeArray(X)
+    padding = np.zeros(desiredLen - len(X))
+    return np.append(X, padding)
+
+def reduceTrailingZeros(X, desiredLen):
+    """Make input into a np.array of desired length, padding with zeros."""
+    X = list(makeArray(X))
+    while X[-1] == 0:
+        X.pop()
+    return np.array(X)
+
+def wrapList(X, desiredLen=2):
+    if not isinstance(X, list):
+        X = [X]
+    if len(X) < desiredLen:
+        X = X + [0]*(desiredLen - len(X))
+    return X
+
+def reduceZero(nums, objs):
+    """nums: array of floats, objs: array of objs (or objs==0), same len."""
+    resNums, resObjs = [], []
+    for i, x in enumerate(nums):
+        if x != 0 and objs[i] != 0:
+            resNums.append(x)
+            resObjs.append(objs[i])
+    return np.array(resNums), resObjs
+            
+def makeRectangle(arr):
+    """Return the minimal rectangular array made by padding rows with 0.
+
+    Parameters
+    ----------
+    arr : array_like : List of lists to be made into a rectangular array.
+
+    Returns
+    -------
+    res: array : Minimal rectangular array.
+    
+    Example(s)
+    ---------
+    >>> arr = [[1,2,3],
+                [1,2]]
+    >>> makeRectangle(arr)
+    >>> [[1. 2. 3.]
+         [1. 2. 0.]]
+    
+    """
+    if isinstance(arr, np.ndarray):
+        return arr
+    lens = [len(row) for row in arr]
+    maxLen = max(lens)
+    res = np.zeros(shape=(len(arr), maxLen))
+    for i, row in enumerate(arr):
+        res[i][:lens[i]] = row
+            
+    return res
